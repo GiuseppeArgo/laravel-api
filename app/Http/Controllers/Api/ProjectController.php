d6ae087bf32085f5e5331a91d1f9a6cbbb3e319f
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-07-02 11:12:00 +0200

    feat: fix to api.php and new route for show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,13 +9,22 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index() {
+        
         $project = Project::all();
-        
+
         $data = [
-            'result' => $project,
-            'success' => true
+            'result' => $project
         ];
-        
+
         return response()->json($data);
+    }
+
+    public function show(string $slug) {
+
+        $project = Project::where('slug', $slug)->first();
+
+        return response() -> json([
+            'results' => $project,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/projects', [ProjectController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
+
+
